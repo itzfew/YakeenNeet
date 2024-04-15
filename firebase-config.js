c58423ac939f954b655928d3144c11da65fbcff1
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-04-15 13:26:50 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDyuYw6oJzE0cJD6WTau4q5mtM8OagssIs",
+    authDomain: "freestudyapp-170e1.firebaseapp.com
+", // Replace with your authDomain
+    projectId: "freestudyapp-170e1",
+    storageBucket: "freestudyapp-170e1.appspot.com", // Use the provided storage bucket
+    messagingSenderId: "58849335299",
+    appId: "1:58849335299:android:2fa6a0430030367f1c5636"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
