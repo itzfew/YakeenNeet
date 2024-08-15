05350dd03aa0a0e4e0b79d3015cbb753dc94709c
Itzfew <113174780+itzfew@users.noreply.github.com>
2024-08-15 17:20:33 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,19 @@ document.addEventListener('DOMContentLoaded', function() {
         lightbox.classList.remove('active');
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const lightbox = document.querySelector('.lightbox');
+    document.querySelectorAll('img').forEach(img => {
+        img.addEventListener('click', function() {
+            if (lightbox) {
+                lightbox.classList.add('active');
+                lightbox.querySelector('img').src = img.src;
+            }
+        });
+    });
+    if (lightbox) {
+        lightbox.addEventListener('click', function() {
+            lightbox.classList.remove('active');
+        });
+    }
+});
